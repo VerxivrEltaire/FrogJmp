515c74ce482f74d2b61f2d259032a721640ba800
VerxivrEltaire <kenneth.iheme@gmail.com>
2022-04-17 18:13:35 +0100

    Project Commit

---
+++ FrogJmp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FrogJmp {
+    class Solution {
+        public int solution(int X, int Y, int D) {
+
+            double result = ((double)Y - (double)X) / (double)D;
+            var count = result % 1 == 0 ? (int)result : result + 1;
+
+            return (int)count;
+        }
+    }
+
+    class MainClass {
+        public static void Main(string[] args) {
+            var sol = new Solution();
+            Console.WriteLine(sol.solution(10, 85, 30));
+        }
+    }
+}
